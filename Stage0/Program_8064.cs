d646a679ec2838ecf057d7463b729fd828245b7c
אליסף אוקנין <elyassaf7@gmail.com>
2024-11-12 20:47:33 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program_8064.cs
@@ -0,0 +1,16 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome_8064();
+        Welcome_3817();
+        Console.ReadKey();
+    }
+    static partial void Welcome_3817();
+    private static void Welcome_8064()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0} Welcome to my first console application", name);
+    }
+}
