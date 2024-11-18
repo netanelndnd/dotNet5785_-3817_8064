f0526f0c5898bfb38cf7328af8e65c10907f8655
אליסף אוקנין <elyassaf7@gmail.com>
2024-11-18 12:45:18 +0200

    look

--- DalList/Config.cs
+++ DalList/Config.cs
@@ -3,37 +3,23 @@
 internal static class Config
 {
 
-    /// <summary>
-    /// Represents the next running identifier for a new call.
-    /// Automatically increments by 1 when a new call is added.
-    /// </summary>
-    internal static int NextCallId { get; }
+    internal const int startCallId = 1;
+    private static int nextCallId = startCallId;
+    internal static int NextCallId { get => nextCallId++; }
 
-    /// <summary>
-    /// Represents the next running identifier for a new assignment.
-    /// Automatically increments by 1 when a new assignment is added.
-    /// </summary>
-    private const int NextAssignmentId { get => NextCallId++; }
+    internal const int startAssigmentId = 1;
+    private static int nextAssigmentId = startAssigmentId;
+    internal static int NextAssigmentId { get => nextAssigmentId++; }
 
-/// <summary>
-/// Simulated system clock that is separate from the actual computer clock.
-/// The administrator can initialize or update this clock as needed.
-/// </summary>
     internal static DateTime Clock { get; set; } = DateTime.Now;
+    internal static TimeSpan RiskRange { get; set; } = TimeSpan.FromHours(2);
 
-/// <summary>
-/// The risk time range from which a call is considered at risk 
-/// as it approaches its required completion time.
-/// </summary>
-    internal static TimeSpan RiskRange { get; }
 
-    /// <summary>
-    /// Default constructor initializes the system clock to the current time.
-    /// </summary>
     internal static void Rest()
     {
         Clock = DateTime.Now; // Initialize the simulated clock to the current system time
-        
+        nextCallId = startCallId;
+        nextAssigmentId = startAssigmentId;
     }
 
 }
---
+++ DalList/VolunteerImplementation.cs
@@ -0,0 +1,43 @@
+﻿namespace Dal;
+using DalApi;
+using DO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+public class VolunteerImplementation : IVolunteer
+{
+    public void Create(Volunteer item)
+    {
+
+        if (DataSource.Volunteers.Contains(item))
+            throw new Exception("This volunteer exists in the database");
+        else
+            DataSource.Volunteers.Add(item);
+    }
+
+    public void Delete(int id)
+    {
+       
+    }
+
+    public void DeleteAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Volunteer? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Volunteer> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Volunteer item)
+    {
+        throw new NotImplementedException();
+    }
+}
